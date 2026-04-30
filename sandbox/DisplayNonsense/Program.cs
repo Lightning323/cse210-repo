64a3d5cc6ba0a915dd6151f75f5fcbd11f54c009
Lightning323 <sam.p.w524@gmail.com>
2026-04-30 14:38:12 -0600

    added stuff

---
+++ sandbox/DisplayNonsense/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace DisplayNonsense;
+
+class Program
+{
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name?");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number?");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static int PromptUserBirthYear()
+    {
+        Console.WriteLine("What is your birth year?");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static int squareNumber(int input)
+    {
+        return input * input;
+    }
+
+    static void DisplayResult()
+    {
+        // Welcome to the program!
+        // Please enter your name: Brother Burton
+        // Please enter your favorite number: 42
+        // Please enter the year you were born: 1990
+        // Brother Burton, the square of your number is 1764
+        // Brother Burton, you will turn 35 this year.
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int birthYr = PromptUserBirthYear();
+        Console.WriteLine($"{name}, your favorite number, squared is is {squareNumber(number)}");
+
+
+        Console.WriteLine($"{name}, you will turn {DateTime.Now.Year - birthYr} this year");
+    }
+
+    static void Main(string[] args)
+    {
+        DisplayResult();
+    }
+}
---
+++ sandbox/TodoList/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace TodoList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, bool> list = [];
+        Console.WriteLine("\nTo List Application." +
+                          "\nPlease enter items in the todo list using the \"add\" and \"remove\" keywords." +
+                          "\nUse the \"view\" keyword to see your list items." +
+                          "\nUse \"check\" or \"uncheck\" to mark items on the todo list as complete." +
+                          "\nUse \"exit\" to exit");
+        while (true)
+        {
+            string input = Console.ReadLine().Trim();
+            if (input.ToLower().Equals("exit"))
+            {
+                break;
+            }
+            else if (input.ToLower().Equals("list"))
+            {
+                foreach (KeyValuePair<string, bool> element in list)
+                {
+                    string status = element.Value ? "DONE" : "INCOMPLETE";
+                    Console.WriteLine($"{element.Key}: \t {status}");
+                }
+            }
+            else
+            {
+                string[] command = input.Split(" ");
+                string keyword = command[0].ToLower();
+                string data = string.Join(" ", command[1..]);
+                Console.WriteLine($"\n{keyword} === {data}");
+                if (keyword.Equals("add"))
+                {
+                    if (!list.ContainsKey(data))
+                    {
+                        list.Add(data, false);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Already added that");
+                    }
+                }
+                else if (keyword.Equals("remove"))
+                {
+                    list.Remove(data);
+                }
+                else if (keyword.Equals("check"))
+                {
+                    if (list.ContainsKey(data))
+                    {
+                        list[data] = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Item not found");
+                    }
+                }
+                else if (keyword.Equals("cncheck"))
+                {
+                    if (list.ContainsKey(data))
+                    {
+                        list[data] = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Item not found");
+                    }
+                }
+            }
+        }
+    }
+}
